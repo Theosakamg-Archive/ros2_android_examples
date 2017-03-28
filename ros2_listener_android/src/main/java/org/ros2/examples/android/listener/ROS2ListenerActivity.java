75eda4d4f194e46ff8b56ffa77b20bb88a1ffbaf
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2017-03-28 22:38:59 +0200

    Commit for Test diff from esteve repository to alfred repository

--- ros2_listener_android/src/main/java/org/ros2/examples/android/listener/ROS2ListenerActivity.java
+++ ros2_listener_android/src/main/java/org/ros2/examples/android/listener/ROS2ListenerActivity.java
@@ -1,4 +1,4 @@
-/* Copyright 2016-2017 Esteve Fernandez <esteve@apache.org>
+/* Copyright 2016 Esteve Fernandez <esteve@apache.org>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,112 +12,139 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.ros2.examples.android.listener;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.os.AsyncTask;
+import android.text.method.ScrollingMovementMethod;
+
+import org.ros2.rcljava.node.topic.Consumer;
 import org.ros2.rcljava.RCLJava;
-import org.ros2.rcljava.consumers.Consumer;
 import org.ros2.rcljava.node.Node;
-import org.ros2.rcljava.subscription.Subscription;
+import org.ros2.rcljava.node.topic.Subscription;
 
-public class ROS2ListenerActivity extends Activity {
+public class ROS2ListenerActivity extends Activity
+{
 
-  private Listener listener;
+    private Listener listener;
 
-  private TextView listenerView;
+    private TextView listenerView;
 
-  private class Listener extends AsyncTask<String, String, String> {
-    @Override
-    protected String doInBackground(final String... text) {
-      Node node = RCLJava.createNode("listener");
-      Subscription<std_msgs.msg.String> chatterSubscription = node.<
-          std_msgs.msg.String>createSubscription(
-            std_msgs.msg.String.class, "chatter",
-            new Consumer<std_msgs.msg.String>() {
+    private class Listener extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... text) {
+            Node node = RCLJava.createNode("listener");
+            Subscription<std_msgs.msg.String> chatter_sub = node.<std_msgs.msg.String>createSubscription(
+                std_msgs.msg.String.class, "chatter",
+                new Consumer<std_msgs.msg.String>() {
+                    @Override
+                    public void accept(std_msgs.msg.String msg) {
+                        publishProgress("I heard: " + msg.getData());
+                    }
+                }
+            );
 
-              @Override
-              public void accept(final std_msgs.msg.String msg) {
-                publishProgress("I heard: " + msg.getData());
-              }
+            while(!isCancelled() && RCLJava.ok()) {
+                RCLJava.spinOnce(node);
             }
-      );
+            return null;
+        }
 
-      while (!isCancelled() && RCLJava.ok()) {
-        RCLJava.spinOnce(node);
-      }
+        @Override
+        protected void onPostExecute(String result) {
+        }
 
-      return null;
+        protected void onProgressUpdate(String... progress) {
+            listenerView.append(progress[0] + "\r\n");
+       }
     }
+
+    private static String logtag = "ROS2ListenerActivity";//for use as the tag when logging
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Button buttonStart = (Button)findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(startListener); // Register the onClick listener with the implementation above
+
+        Button buttonStop = (Button)findViewById(R.id.buttonStop);
+        buttonStop.setOnClickListener(stopListener); // Register the onClick listener with the implementation above
+        buttonStop.setEnabled(false);
+
+        listenerView = (TextView)findViewById(R.id.listenerView);
+        listenerView.setMovementMethod(new ScrollingMovementMethod());
+        RCLJava.rclJavaInit();
+    }
+
+    //Create an anonymous implementation of OnClickListener
+    private OnClickListener startListener = new OnClickListener() {
+        public void onClick(View v) {
+            Log.d(logtag,"onClick() called - start button");
+            Toast.makeText(ROS2ListenerActivity.this, "The Start button was clicked.", Toast.LENGTH_LONG).show();
+            Log.d(logtag,"onClick() ended - start button");
+            Button buttonStart = (Button)findViewById(R.id.buttonStart);
+            Button buttonStop = (Button)findViewById(R.id.buttonStop);
+            buttonStart.setEnabled(false);
+            buttonStop.setEnabled(true);
+            listener = new Listener();
+            listener.execute("Hello ROS2 from Android");
+        }
+    };
+
+    // Create an anonymous implementation of OnClickListener
+    private OnClickListener stopListener = new OnClickListener() {
+        public void onClick(View v) {
+            Log.d(logtag,"onClick() called - stop button");
+            Toast.makeText(ROS2ListenerActivity.this, "The Stop button was clicked.", Toast.LENGTH_LONG).show();
+            listener.cancel(true);
+            Button buttonStart = (Button)findViewById(R.id.buttonStart);
+            Button buttonStop = (Button)findViewById(R.id.buttonStop);
+            buttonStart.setEnabled(true);
+            buttonStop.setEnabled(false);
+            Log.d(logtag,"onClick() ended - stop button");
+        }
+    };
+
 
     @Override
-    protected void onPostExecute(final String result) {
+    protected void onStart() {//activity is started and visible to the user
+        Log.d(logtag,"onStart() called");
+        super.onStart();
     }
+    @Override
+    protected void onResume() {//activity was resumed and is visible again
+        Log.d(logtag,"onResume() called");
+        super.onResume();
 
-    protected void onProgressUpdate(final String... progress) {
-      listenerView.append(progress[0] + "\r\n");
     }
-  }
+    @Override
+    protected void onPause() { //device goes to sleep or another activity appears
+        Log.d(logtag,"onPause() called");//another activity is currently running (or user has pressed Home)
+        super.onPause();
 
-  private static String logtag = "ROS2ListenerActivity";
-
-  /** Called when the activity is first created. */
-  @Override
-  public final void onCreate(final Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
-
-    Button buttonStart = (Button) findViewById(R.id.buttonStart);
-    buttonStart.setOnClickListener(startListener);
-
-    Button buttonStop = (Button) findViewById(R.id.buttonStop);
-    buttonStop.setOnClickListener(stopListener);
-    buttonStop.setEnabled(false);
-
-    listenerView = (TextView) findViewById(R.id.listenerView);
-    listenerView.setMovementMethod(new ScrollingMovementMethod());
-
-    RCLJava.rclJavaInit();
-  }
-
-  //Create an anonymous implementation of OnClickListener
-  private OnClickListener startListener = new OnClickListener() {
-    public void onClick(final View view) {
-      Log.d(logtag, "onClick() called - start button");
-      Toast.makeText(ROS2ListenerActivity.this, "The Start button was clicked.",
-        Toast.LENGTH_LONG).show();
-      Log.d(logtag, "onClick() ended - start button");
-      Button buttonStart = (Button) findViewById(R.id.buttonStart);
-      Button buttonStop = (Button) findViewById(R.id.buttonStop);
-      buttonStart.setEnabled(false);
-      buttonStop.setEnabled(true);
-      listener = new Listener();
-      listener.execute("Hello ROS2 from Android");
     }
-  };
+    @Override
+    protected void onStop() { //the activity is not visible anymore
+        Log.d(logtag,"onStop() called");
+        super.onStop();
 
-  // Create an anonymous implementation of OnClickListener
-  private OnClickListener stopListener = new OnClickListener() {
-    public void onClick(final View view) {
-      Log.d(logtag, "onClick() called - stop button");
-      Toast.makeText(ROS2ListenerActivity.this, "The Stop button was clicked.",
-        Toast.LENGTH_LONG).show();
-      listener.cancel(true);
-      Button buttonStart = (Button) findViewById(R.id.buttonStart);
-      Button buttonStop = (Button) findViewById(R.id.buttonStop);
-      buttonStart.setEnabled(true);
-      buttonStop.setEnabled(false);
-      Log.d(logtag, "onClick() ended - stop button");
     }
-  };
+    @Override
+    protected void onDestroy() {//android has killed this activity
+        Log.d(logtag,"onDestroy() called");
+        super.onDestroy();
+    }
 }
